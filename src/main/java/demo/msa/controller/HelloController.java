7fd3de75782af1fcb19b1fcc4215e06abde0b62e
clp <972385283@qq.com>
2017-08-21 10:30:50 +0800

    能够解析jsp页面

--- src/main/java/demo/msa/controller/HelloController.java
+++ src/main/java/demo/msa/controller/HelloController.java
@@ -1,17 +1,33 @@
 package demo.msa.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 /**
  * Created by jinli on 2017/7/31.
  */
-@RestController
+@Controller
 public class HelloController {
 
-    @RequestMapping(method= RequestMethod.GET,path="/hello")
+    @GET
+    @RequestMapping("/hello")
     public String Hello(){
-        return "hello";
+        return "/templates/test";
+    }
+
+    @GET
+    @ResponseBody
+    @RequestMapping("/hello2")
+    public String Hello2(){
+        return "Hello";
     }
 }
